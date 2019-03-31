4d539728ff36c57ccae40d0d5be793a8e88a955a
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2019-03-31 23:31:33 +0400

    Added data and new routes to main.py

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
+posts = [
+  {
+    'author': 'Amiraslan Bakhshili',
+    'title': 'Blog post 1',
+    'content': 'First post content',
+    'date_posted': 'April 20, 2018'
+  },
+  {
+    'author': 'Elik Melik',
+    'title': 'Blog post 2',
+    'content': 'Second post content',
+    'date_posted': 'April 21, 2018'
+  }
+]
+
+
 @app.route("/")
+@app.route("/home")
 def hello():
-    return "<h1>Amiraslan!</h1>"
+  return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+  return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
